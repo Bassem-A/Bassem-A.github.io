ba12ceda274ad974710b848c0ba85dbcaa51278c
Bassem-A <2190000057@iau.edu.sa>
2021-07-10 07:38:47 +0300

    added firebase

--- myscript.js
+++ myscript.js
@@ -1,6 +1,15 @@
 
 
 
+function SignInG()
+{
+     const auth = firebase.auth();
+     const provider = new firebase.auth.GoogleAuthProvider();
+     auth.signInWithRedirect(provider);
+    
+}
+
+
 function SHprojects()
 {
     var pjBtn = document.getElementById("pjs");
@@ -20,6 +29,8 @@ function Theme_Change()
     var mainbody = document.getElementById("mainbody");
     var btn = document.getElementById("PJbutton");
     var btn2 = document.getElementById("theme_button");
+    var btn3 = document.getElementById("SignIn");
+
     
     var redBG = "rgb(219, 35, 73)";
     
@@ -32,12 +43,15 @@ function Theme_Change()
         mainbody.style.setProperty("background-color", "#23272a");
         btn.style.setProperty("background-color", "#23272a");
         btn2.style.setProperty("background-color", "#DB2349");
+        btn3.style.setProperty("background-color", "#23272a");
+
         
     }
     else{
         mainbody.style.setProperty("background-color", "#DB2349");
         btn.style.setProperty("background-color", "#DB2349");
         btn2.style.setProperty("background-color", "#23272a");
+        btn3.style.setProperty("background-color", "#DB2349");
     }
     
 
